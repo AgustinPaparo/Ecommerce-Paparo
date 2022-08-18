cb5c75dd65916f0cc41651bc35815fb92a866268
Agustin <aguspaparo@gmail.com>
2022-08-17 22:26:02 -0300

    Primer pagina REACT

--- src/App.js
+++ src/App.js
@@ -2,23 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let imagen ="hamburguesa.png";
+  let titulo = "Doble cuarto de libra";
+  let calorias = "771kcal";
+  let descripcion = "Imaginá la sensación del clásico Cuarto de Libra. Imaginalo con un medallón de deliciosa carne 100% vacuna, queso cheddar, cebolla, kétchup y mostaza ¿Listo? Ahora duplicá esa sensación. Ya tenés el Doble Cuarto en la cabeza.";
+  const producto ={ id:1 , image:imagen , calories:calorias, title:titulo , description:descripcion }
+  
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='row my-5'>
+        <div className='col-md-4 offset-md-2'>
+          <img src={producto.image} alt={producto.title} className='img-fluid'/>
+        </div>
+        <div className='col-md-4'>
+          <h1 className='text-center'>{producto.title}</h1>
+          <p>{producto.calories}</p>
+          <p className='text-center'>{producto.description}</p>
+        </div>
+      </div>
+
     </div>
+
   );
 }
 
